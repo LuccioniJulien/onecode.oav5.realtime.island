b0985560da598adb56a7ca2ba50212e6ad2d003a
Unknown <julien.luccioni@protonmail.com>
2018-11-18 13:26:26 +0100

    last game

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import App from "./App";
-import Magic from "./Magic";
 import Home from "./Home";
 
 const MyRoute = () => (
@@ -9,7 +8,6 @@ const MyRoute = () => (
 		<App>
 			<Switch>
 				<Route exact path="/" component={Home} />
-				<Route exact path="/Magic" component={Magic} />
 			</Switch>
 		</App>
 	</Router>
